42cedd85c9f8dc97f001f525a3d81ce2efc83800
0art0 <18333981+0art0@users.noreply.github.com>
2025-04-15 18:48:13 +0100

    Refactor code for querying server

--- Kimina/Querying.lean
+++ Kimina/Querying.lean
@@ -30,66 +30,49 @@ def getParams : M Json :=
   }
 
 def queryRaw (prompt : String) : ExceptT String M String := do
-  let serverUrl ← getServerUrl
-  let params ← liftM (m := M) getParams
-
-  let tokenizeUrl := serverUrl ++ "tokenize"
   let tokenizeData := json% {
     text_input: $(prompt),
     return_ids: true
   }
-
-  let tokenizeOutput ← IO.Process.output {
-    cmd := "curl",
-    args := #[
-      "-X", "POST",
-      "--header", "Content-Type: application/json",
-      "--data", tokenizeData.compress,
-      "-s", tokenizeUrl
-    ]
-  }
-  let tokenizeJson ← Json.parse tokenizeOutput.stdout
+  let tokenizeJson ← queryServer (endpoint := "tokenize") tokenizeData
 
   let inputIds ← tokenizeJson.getObjVal? "tokens_ids"
   let attentionMask ← tokenizeJson.getObjVal? "attention_mask"
+  let params ← liftM (m := M) getParams
 
-  let forwardServerUrl := serverUrl ++ "forward"
   let forwardData := json% {
     inputs: $(inputIds),
     attention_mask: $(attentionMask),
     parameters: $(params)
   }
-  let forwardOutput ← IO.Process.output {
-    cmd := "curl",
-    args := #[
-      "-X", "POST",
-      "--header", "Content-Type: application/json",
-      "--data", forwardData.compress,
-      "-s", forwardServerUrl
-    ]
-  }
-  let forwardJson ← Json.parse forwardOutput.stdout
+  let forwardJson ← queryServer (endpoint := "forward") forwardData
+
   let output ← forwardJson.getObjVal? "output"
 
-  let detokenizeUrl := serverUrl ++ "detokenize"
   let detokenizeData := json% {
     "token_ids": $(output),
     "skip_special_tokens": true,
     "cleanup_tokenization_spaces": true
   }
+  let detokenizeJson ← queryServer (endpoint := "detokenize") detokenizeData
 
-  let detokenizeOutput ← IO.Process.output {
-    cmd := "curl",
-    args := #[
-      "-X", "POST",
-      "--header", "Content-Type: application/json",
-      "--data", detokenizeData.compress,
-      "-s", detokenizeUrl
-    ]
-  }
-  let detokenizeJson ← Json.parse detokenizeOutput.stdout
   let generatedText ← detokenizeJson.getObjValAs? String "text"
-
   return generatedText
+where
+  queryServer (endpoint : String) (data : Json) : ExceptT String M Json := do
+    let response ← IO.Process.output {
+      cmd := "curl",
+      args := #[
+        "-X", "POST",
+        "--header", "Content-Type: application/json",
+        "--data", data.compress,
+        "-s", (← getServerUrl) ++ endpoint
+      ]
+    }
+    if response.exitCode != 0 then
+      throw s!"Error querying server: {response.stderr}"
+    let responseJson ← Json.parse response.stdout
+    IO.println s!"Response at endpoint {endpoint}:\n{responseJson.pretty}"
+    return responseJson
 
 end Kimina
