34df00517f9c459fd11a42c1ef62b69c1c46fb42
0art0 <18333981+0art0@users.noreply.github.com>
2025-04-17 05:20:45 +0100

    Trim input file

--- Kimina/Tactic.lean
+++ Kimina/Tactic.lean
@@ -9,7 +9,7 @@ elab stx:"kimina" : tactic => do
   let file := fileMap.source
   let .some pos := stx.getPos? (canonicalOnly := true)
     | throwError "Failed to infer current position in file."
-  let filePrefix := file.extract {} pos
+  let filePrefix := file.extract {} pos |>.dropRightWhile (fun c ↦ c = ' ' || c = '\t')
   let prompt := constructPromptText filePrefix
   let cleanPrompt := constructPromptText filePrefix (startToken := "") (endToken := "")
   let response ← queryModel prompt
